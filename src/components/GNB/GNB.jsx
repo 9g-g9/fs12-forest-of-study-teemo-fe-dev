e07a0a6f31e59af29763b6d8bace27d2581937f6
gptjd0204 <gptjd0204@naver.com>
2026-04-09 19:22:36 +0900

    feat: 메인과 생성 페이지에서만 버튼 렌더링

--- src/components/GNB/GNB.jsx
+++ src/components/GNB/GNB.jsx
@@ -1,17 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/images/img_logo.png';
 import './GNB.css';
 
 const GNB = () => {
+  const location = useLocation();
+
+  const isCreateBtn = ['/study', '/study/create'].includes(location.pathname);
+
   return (
     <header className="gnbWrapper">
       <nav className="gnbContainer">
         <Link to="/study">
           <img src={logo} alt="공부의 숲" className="logo" />
         </Link>
-        <Link to="/study/create" className="btn createBtn">
-          스터디 만들기
-        </Link>
+        {isCreateBtn && (
+          <Link to="/study/create" className="btn createBtn">
+            스터디 만들기
+          </Link>
+        )}
       </nav>
     </header>
   );
