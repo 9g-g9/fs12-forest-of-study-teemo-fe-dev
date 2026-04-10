f2bfc18a26160fb02fc9c9e9ae73572bfcf5740e
gptjd0204 <gptjd0204@naver.com>
2026-04-10 10:18:36 +0900

    refactor: url path 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,16 @@ import './Header.css';
 const Header = () => {
   const location = useLocation();
 
-  const isCreateBtn = ['/study', '/study/create'].includes(location.pathname);
+  const isCreateBtn = ['/', '/create'].includes(location.pathname);
 
   return (
     <header className="header-wrapper">
       <nav className="gnb-container">
-        <Link to="/study">
+        <Link to="/">
           <img src={logo} alt="공부의 숲" className="logo" />
         </Link>
         {isCreateBtn && (
-          <Link to="/study/create" className="btn create-btn">
+          <Link to="/create" className="btn create-btn">
             스터디 만들기
           </Link>
         )}
