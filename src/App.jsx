c48ced7b30effaade21495c61e82ff96ddf3057c
9g-g9 <nayun12375@gmail.com>
2026-04-10 17:15:53 +0900

    feat: button 기본 css 및 구조 정의

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import './App.css';
 import './styles/pattern.css';
 import './styles/reset.css';
 import Header from './components/Header/Header';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div>
       <Header />
+      <Button
+        btnTxt={'버튼'}
+        btnStyle={'btnDefault'}
+        btnType={'button'}
+      ></Button>
     </div>
   );
 }
