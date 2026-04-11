1f5ac0e03d541e0e4e4f18c4b27462faee66279b
hogu-giriboy <choo6078@kakao.com>
2026-04-11 14:07:45 +0900

    feat: 레이아웃 구조 초기 설정 및 Modal props 구조 추가

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,9 @@ const Modal = ({ title, children }) => {
     <>
       <div className={styles.overlay}>
         <div className={styles.container}>
+          <div className={styles.header}>{title}</div>
           <div className={styles.body}>{children}</div>
+          <div className={styles.footer}></div>
         </div>
       </div>
     </>
