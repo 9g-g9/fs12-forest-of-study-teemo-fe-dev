db5b20e25dc269314f707bcbba02f4d002ebd0c2
hogu-giriboy <choo6078@kakao.com>
2026-04-12 15:34:54 +0900

    refactor: HabitModal 레이아웃 구조 및 반응형 스타일 개선

--- src/components/Modal/HabitModal/HabitModal.jsx
+++ src/components/Modal/HabitModal/HabitModal.jsx
@@ -3,14 +3,18 @@ import styles from './HabitModal.module.css';
 
 const HabitModal = ({ title, children }) => {
   return (
-    <ModalLayout>
-      <div className={styles.header}>{title}</div>
-      <div className={styles.body}>{children}</div>
-      <div className={styles.footer}>
-        <button className="btn">취소</button>
-        <button className="btn">수정 완료</button>
-      </div>
-    </ModalLayout>
+    <>
+      <ModalLayout>
+        <div className={styles.header}>
+          <div className={styles.title}>{title}</div>
+        </div>
+        <div className={styles.body}>{children}</div>
+        <div className={styles.footer}>
+          <button className="btn">취소</button>
+          <button className="btn">수정 완료</button>
+        </div>
+      </ModalLayout>
+    </>
   );
 };
 
