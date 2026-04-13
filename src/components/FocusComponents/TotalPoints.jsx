3389d80a6475e81c3f5dff165744f3b27e8b6a67
gptjd0204 <gptjd0204@naver.com>
2026-04-13 18:40:16 +0900

    design: 중복 icon 파일 수정

--- src/components/FocusComponents/TotalPoints.jsx
+++ src/components/FocusComponents/TotalPoints.jsx
@@ -1,12 +1,12 @@
 import styles from '../../pages/TodayFocusPage/TodayFocus.module.css';
-import icPoint from '../../assets/icons/ic_point.svg';
+import icLeaf from '../../assets/icons/ic_leaf.svg';
 
 const TotalPoints = () => {
   return (
     <div className={styles.pointContainer}>
       <p>현재까지 획득한 포인트</p>
       <div className={styles.point}>
-        <img src={icPoint} />
+        <img src={icLeaf} />
         <p>310p 획득</p>
       </div>
     </div>
