3c547e7d9a94409f8ea8afdf9e19fd059c80d523
gptjd0204 <gptjd0204@naver.com>
2026-04-14 11:48:22 +0900

    refactor: 총합 포인트 공통 컴포넌트로 분리

---
+++ src/components/TotalPoint/TotalPoint.jsx
@@ -0,0 +1,27 @@
+import styles from './TotalPoint.module.css';
+import icPoint from '../../assets/icons/ic_point.svg';
+
+/* ----------------------------------
+            총합 포인트 컴포넌트
+  -----------------------------------
+
+  size = TotalPoint의 크기 (m, default는 s)
+  theme = TotalPoint의 테마 (dark, default는 white)
+
+  (ex: <TotalPoint size={'m'} theme={'dark'} />
+       <TotalPoint size={'m'}/> 
+       <TotalPoint /> )
+*/
+
+const TotalPoint = ({ size, theme }) => {
+  return (
+    <div
+      className={`${size === 'm' ? styles.point : styles.pointSmall} ${theme === 'dark' ? styles.dark : ''}`}
+    >
+      <img src={icPoint} alt="총합 포인트 아이콘" />
+      <p>310p 획득</p>
+    </div>
+  );
+};
+
+export default TotalPoint;
