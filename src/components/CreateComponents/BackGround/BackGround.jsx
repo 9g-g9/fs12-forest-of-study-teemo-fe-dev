cd691ed36d2506bdb3f2858d82fd17a25165e302
DevelopWoo <kksw7000@naver.com>
2026-04-14 14:31:25 +0900

    rename : background 이미지 이름수정 경로수정

--- src/components/CreateComponents/BackGround/BackGround.jsx
+++ src/components/CreateComponents/BackGround/BackGround.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import styles from './BackGround.module.css';
 
-import img1 from '../../../assets/images/img1.png.png';
-import img2 from '../../../assets/images/img2.png.png';
-import img3 from '../../../assets/images/img3.png.png';
-import img4 from '../../../assets/images/img4.png.png';
-import img5 from '../../../assets/images/img5.png.png';
-import img6 from '../../../assets/images/img6.png.png';
-import img7 from '../../../assets/images/img7.png.png';
-import img8 from '../../../assets/images/img8.png.png';
+import img1 from '../../../assets/images/background_green.png';
+import img2 from '../../../assets/images/background_yellow.png';
+import img3 from '../../../assets/images/background_blue.png';
+import img4 from '../../../assets/images/background_pink.png';
+import img5 from '../../../assets/images/img5.png';
+import img6 from '../../../assets/images/img6.png';
+import img7 from '../../../assets/images/img7.png';
+import img8 from '../../../assets/images/img8.png';
 
 const BackGround = () => {
   const images = [img1, img2, img3, img4, img5, img6, img7, img8];
