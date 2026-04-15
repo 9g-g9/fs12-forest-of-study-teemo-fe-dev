3ad137bb4c740742b51e1e47e31ec6e93849f01f
gptjd0204 <gptjd0204@naver.com>
2026-04-15 22:29:55 +0900

    feat: 목표 시간 설정 API 연동 및 타이머 생성, 조회 API 분리

--- src/services/TimerService.js
+++ src/services/TimerService.js
@@ -1,18 +1,49 @@
-export const upsertTimer = async (studyId) => {
+export const getTimer = async (studyId) => {
   try {
-    const res = await fetch(`http://localhost:8080/api/timers`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        studyId,
-      }),
-    });
-    if (!res.ok) {
-      throw new Error('데이터 로딩에 실패했습니다');
-    }
-    const { data } = await res.json();
+    const response = await fetch(`http://localhost:8080/api/timers/${studyId}`);
+    const { data } = await response.json();
 
     return data.timer;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createTimer = async (studyId) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/timers/${studyId}`,
+      { method: 'POST' },
+    );
+    if (!response.ok) {
+      throw new Error('데이터 응답에 실패했습니다');
+    }
+    const { data } = await response.json();
+
+    return data.timer;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateTargetDuration = async (studyId, targetDuration) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/timers/${studyId}/target-duration`,
+      {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          targetDuration,
+        }),
+      },
+    );
+    if (!response.ok) {
+      throw new Error('데이터 응답에 실패했습니다');
+    }
+    // const { data } = await res.json();
+
+    // return data.timer;
   } catch (error) {
     console.error(error);
   }
