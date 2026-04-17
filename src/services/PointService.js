c2415db964f8c0db65696bb7782bd135ed69cc7f
gptjd0204 <gptjd0204@naver.com>
2026-04-17 18:13:38 +0900

    chore: FocusComponents 파일 이동 및 API URL 환경 변수 설정

--- src/services/PointService.js
+++ src/services/PointService.js
@@ -1,6 +1,8 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const getTotalPoint = async (id) => {
   try {
-    const res = await fetch(`http://localhost:8080/api/points/${id}`);
+    const res = await fetch(`${apiUrl}/api/points/${id}`);
     if (!res.ok) {
       throw new Error('데이터 로딩에 실패했습니다');
     }
